34f96ad01bdd053dfddea404bc6d0503ae26041f
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-01 13:32:17 +0530

    Implement CourseController and add initial methods for course management Add LecturesController, LecturesDto, LecturesEntity, LecturesRepository, LecturesService, and LectureServiceImpl as part of lecture management feature

--- src/main/java/edu/icet/ecom/controller/CourseController.java
+++ src/main/java/edu/icet/ecom/controller/CourseController.java
@@ -1,4 +1,45 @@
 package edu.icet.ecom.controller;
 
-public class CourseController {
+import edu.icet.ecom.dto.CourseDto;
+import edu.icet.ecom.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/course")
+public class CourseController{
+
+    final CourseService courseService;
+
+    @PostMapping("/add")
+    public void addCourse(@RequestBody CourseDto courseDto) {
+        courseService.addCourse(courseDto);
+    }
+
+
+    public void updateCourse(CourseDto courseDto) {
+
+    }
+
+
+    public void deleteCourse(Long id) {
+
+    }
+
+
+    public CourseDto searchByID(Long id) {
+        return null;
+    }
+
+
+    public List<CourseDto> getAll() {
+        return List.of();
+    }
 }
