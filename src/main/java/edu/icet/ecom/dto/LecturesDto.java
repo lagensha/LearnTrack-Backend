be8aa358875d0a0f6975e97d16db0744f7d6520d
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-02 09:11:32 +0530

    Add LecturesDto and LecturesEntity classes with necessary fields and annotations

--- src/main/java/edu/icet/ecom/dto/LecturesDto.java
+++ src/main/java/edu/icet/ecom/dto/LecturesDto.java
@@ -1,4 +1,17 @@
 package edu.icet.ecom.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LecturesDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String courseId;
+
 }
--- src/main/java/edu/icet/ecom/entity/LecturesEntity.java
+++ src/main/java/edu/icet/ecom/entity/LecturesEntity.java
@@ -1,4 +1,22 @@
 package edu.icet.ecom.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class LecturesEntity {
+    @Id
+    @GeneratedValue (strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String courseId;
 }
