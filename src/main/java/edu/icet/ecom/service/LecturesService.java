2d9201001527a015fd8642388f2c5d3225699262
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-02 09:21:04 +0530

    Add methods for lecture management in LecturesService interface

--- src/main/java/edu/icet/ecom/service/LecturesService.java
+++ src/main/java/edu/icet/ecom/service/LecturesService.java
@@ -1,4 +1,10 @@
 package edu.icet.ecom.service;
 
+import edu.icet.ecom.dto.LecturesDto;
+
 public interface LecturesService {
+    void addLecture(LecturesDto lecturesDto);
+    void updateLecture(LecturesDto lecturesDto);
+    void deleteLecture(Long id);
+    LecturesDto searchByID(Long id);
 }
