137a15ec9282c89a59df0f37efe64f802b0bb0f5
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-05 16:06:07 +0530

    Add EnrollmentsController with REST annotations and integrate EnrollmentsService

--- src/main/java/edu/icet/ecom/controller/EnrollmentsController.java
+++ src/main/java/edu/icet/ecom/controller/EnrollmentsController.java
@@ -2,14 +2,22 @@ package edu.icet.ecom.controller;
 
 import edu.icet.ecom.dto.EnrollmentsDto;
 import edu.icet.ecom.service.EnrollmentsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/api/v1/enrollments")
+@RequiredArgsConstructor
 public class EnrollmentsController {
 
+    final private EnrollmentsService enrollmentsService;
 
     public void addEnrollment(EnrollmentsDto enrollmentsDto) {
-
+        enrollmentsService.addEnrollment(enrollmentsDto);
     }
 
 
