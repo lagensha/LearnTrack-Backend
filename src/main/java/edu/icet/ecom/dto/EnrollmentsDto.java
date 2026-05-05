208ee08355e1afb1da9750f304c9afa30a0a9488
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-05 15:54:06 +0530

    Add fields to EnrollmentsDto for enrollment details

--- src/main/java/edu/icet/ecom/dto/EnrollmentsDto.java
+++ src/main/java/edu/icet/ecom/dto/EnrollmentsDto.java
@@ -1,4 +1,14 @@
 package edu.icet.ecom.dto;
 
+import lombok.Data;
+
+
 public class EnrollmentsDto {
+    private  Long enrollmentId;
+    private Long courseId;
+    private Long studentId;
+    private Data enrollmentDate;
+    private boolean status;
+    private String grade;
+    private String completionDate;
 }
