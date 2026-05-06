44ddd1ec94d5ef27aeb470407c9a5c2ee5a910a2
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-06 09:38:17 +0530

    Implement CertificateService methods for adding, updating, deleting, and retrieving certificates

--- src/main/java/edu/icet/ecom/service/impl/CertificateServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/impl/CertificateServiceImpl.java
@@ -1,4 +1,44 @@
 package edu.icet.ecom.service.impl;
 
-public class CertificateServiceImpl {
+import edu.icet.ecom.dto.CertificateDto;
+import edu.icet.ecom.entity.CertificateEntity;
+import edu.icet.ecom.repository.CertificateRepository;
+import edu.icet.ecom.service.CertificateService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class CertificateServiceImpl implements CertificateService {
+    final CertificateRepository certificateRepository;
+    final ModelMapper modelMapper;
+
+    @Override
+    public void addCertificate(CertificateDto certificateDto) {
+        CertificateEntity certificateEntity=modelMapper.map(certificateDto,CertificateEntity.class);
+        certificateRepository.save(certificateEntity);
+    }
+
+    @Override
+    public void updateCertificate(CertificateDto certificateDto) {
+
+    }
+
+    @Override
+    public void deleteCertificate(Long id) {
+
+    }
+
+    @Override
+    public CertificateDto searchByID(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<CertificateDto> getAll() {
+        return null;
+    }
 }
