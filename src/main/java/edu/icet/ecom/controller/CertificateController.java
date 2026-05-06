5f54f2ab97ce44a00d3b670f8219dcf35ca08179
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-06 09:43:15 +0530

    Add CertificateController with methods for adding, updating, deleting, and retrieving certificates

--- src/main/java/edu/icet/ecom/controller/CertificateController.java
+++ src/main/java/edu/icet/ecom/controller/CertificateController.java
@@ -1,4 +1,45 @@
 package edu.icet.ecom.controller;
 
-public class CertificateController {
+import edu.icet.ecom.dto.CertificateDto;
+import edu.icet.ecom.service.CertificateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/certificate")
+@RestController
+public class CertificateController{
+
+    final CertificateService certificateService;
+
+    @PostMapping("/add")
+    public void addCertificate(@RequestBody CertificateDto certificateDto) {
+        certificateService.addCertificate(certificateDto);
+    }
+
+
+    public void updateCertificate(CertificateDto certificateDto) {
+
+    }
+
+
+    public void deleteCertificate(Long id) {
+
+    }
+
+
+    public CertificateDto searchByID(Long id) {
+        return null;
+    }
+
+
+    public List<CertificateDto> getAll() {
+        return List.of();
+    }
 }
