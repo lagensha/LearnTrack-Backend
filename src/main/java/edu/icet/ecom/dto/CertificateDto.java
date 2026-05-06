5e390097144decd48744af4fa6b3af9837055753
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-06 09:29:48 +0530

    Add Certificate DTO and Entity with necessary fields and annotations

--- src/main/java/edu/icet/ecom/dto/CertificateDto.java
+++ src/main/java/edu/icet/ecom/dto/CertificateDto.java
@@ -1,4 +1,16 @@
 package edu.icet.ecom.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CertificateDto {
+    private Long id;
+    private String name;
+    private String description;
+    private String studentId;
+    private String courseId;
 }
--- src/main/java/edu/icet/ecom/entity/CertificateEntity.java
+++ src/main/java/edu/icet/ecom/entity/CertificateEntity.java
@@ -1,4 +1,23 @@
 package edu.icet.ecom.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class CertificateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String studentId;
+    private String courseId;
 }
