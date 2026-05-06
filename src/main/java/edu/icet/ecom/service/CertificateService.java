3f63a5ec3400113157d12dd697f191425e0287e0
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-05-06 09:33:11 +0530

    Add methods for managing certificates in CertificateService interface

--- src/main/java/edu/icet/ecom/service/CertificateService.java
+++ src/main/java/edu/icet/ecom/service/CertificateService.java
@@ -1,4 +1,14 @@
 package edu.icet.ecom.service;
 
+import edu.icet.ecom.dto.CertificateDto;
+
+import java.util.List;
+
 public interface CertificateService {
+    void addCertificate(CertificateDto certificateDto);
+    void updateCertificate(CertificateDto certificateDto);
+    void deleteCertificate(Long id);
+    CertificateDto searchByID(Long id);
+    List<CertificateDto> getAll();
+
 }
